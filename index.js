b85a347066f267dfc91d57962378c0d8fa318a76
Nathalia Cardozo <nathaliacardozosilva@gmail.com>
2022-02-28 19:45:38 -0300

    menu de carrinho finalizado

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const botaoCarrinho = document.querySelector(".cabecalho__carrinho");
 const botaoFechar = document.querySelector(".carrinho__botao-fechar--ativo");
 const carrinho = document.querySelector(".carrinho");
+const body = document.querySelector(".fundo-escuro");
 
 botaoCarrinho.addEventListener('click', () => {
     carrinho.classList.toggle('carrinho--ativo')
+    body.classList.toggle('fundo-escuro--ativo')
 })
 
 botaoFechar.addEventListener('click', () => {
     carrinho.classList.toggle('carrinho--ativo')
+    body.classList.toggle('fundo-escuro--ativo')
 })
