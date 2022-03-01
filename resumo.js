934ea27be621a722d52771d1d4088fb8c8046cea
Nathalia Cardozo <nathaliacardozosilva@gmail.com>
2022-03-01 15:39:29 -0300

    preenchimento automático do endereço

--- resumo.js
+++ resumo.js
@@ -7,3 +7,27 @@ botaoFinalizar.addEventListener('click', (e) => {
     modal.classList.toggle('modal--ativo');
     fundoEscuro.classList.toggle('fundo-escuro--ativo');
 })
+
+function buscaCEP() {
+    let cep = document.querySelector(".input--cep");
+    let valorCEP = cep.value;
+    let url = 'http://viacep.com.br/ws/' + valorCEP + '/json'
+    let xhr = new XMLHttpRequest(); 
+    
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function() { 
+        if (xhr.readyState == 4) { 
+        if (xhr.status = 200) preencheCampos(JSON.parse(xhr.responseText)); } 
+    } 
+    xhr.send();
+}
+
+function preencheCampos(json) { 
+    let logradouro = document.querySelector(".input--logradouro");
+    let cidade = document.querySelector(".input--cidade");
+    let estado = document.querySelector(".input--estado")
+
+    logradouro.value = json.logradouro;
+    cidade.value = json.localidade; 
+    estado.value = json.uf; 
+}
